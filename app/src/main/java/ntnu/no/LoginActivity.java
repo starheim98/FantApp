db16e718684b8d9c5acab4bcf5949fca19beeb09
Andreas <andreas.hernas@gmail.com>
2021-09-05 13:56:54 +0200

    created toolbar

---
+++ app/src/main/java/ntnu/no/LoginActivity.java
@@ -0,0 +1,18 @@
+package ntnu.no;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+    }
+}
