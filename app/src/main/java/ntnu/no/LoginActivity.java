8169454664241930421cde1ffbf5b02868676f15
Andreas <andreas.hernas@gmail.com>
2021-09-05 14:41:41 +0200

    link redirect to sign up page

--- app/src/main/java/ntnu/no/LoginActivity.java
+++ app/src/main/java/ntnu/no/LoginActivity.java
@@ -3,7 +3,10 @@ package ntnu.no;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,7 +15,17 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        TextView signUpLink = findViewById(R.id.signUpLink);
+        signUpLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signUpSwitch();
+            }
+        });
+    }
+
+    private void signUpSwitch() {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
