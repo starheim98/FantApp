0f200b41fdcac4d208e7a3c3f50e2043dfa038f4
Andreas <andreas.hernas@gmail.com>
2021-09-20 09:24:47 +0200

    ItemDetailPage in progress

--- app/src/main/java/ntnu/no/ViewPagerAdapter.java
+++ app/src/main/java/ntnu/no/ViewPagerAdapter.java
@@ -1,45 +1,72 @@
 package ntnu.no;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
 
+import ntnu.no.model.Item;
 import ntnu.no.model.Photo;
 
-public class ViewPagerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewPageViewHolder> {
+public class ViewPagerAdapter extends RecyclerView.Adapter<ViewPagerAdapter.ViewPageViewHolder> {
+
+
+    //    private final String url = "http://10.22.190.200:8080/api/";
+    private final String url = "http://192.168.0.120:8080/api/";
+    private final String imageUrl = url + "fant/photo/";
+
     private ArrayList<Photo> photos;
 
     public ViewPagerAdapter(ArrayList<Photo> photos){
         this.photos = photos;
     }
 
+    public class ViewPageViewHolder extends RecyclerView.ViewHolder {
+        private ImageView image;
 
-    public class ViewPagerViewHolder extends RecyclerView.ViewHolder{
-
-        public ViewPagerViewHolder(final View view){
+        public ViewPageViewHolder(final View view){
             super(view);
+            image = view.findViewById(R.id.itemDetailImage);
         }
     }
 
     @NonNull
     @Override
-    public RecyclerAdapter.ViewPageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewPageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_detail_image, parent, false);
+        return new ViewPageViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerAdapter.ViewPageViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewPageViewHolder holder, int position) {
+        if(photos.size() > 0){
+            Photo currentImage = photos.get(position);
+            String subPath = currentImage.getSubPath();
+
+            Picasso.get()
+                    .load(imageUrl + subPath)
+                    .fit()
+                    .centerCrop()
+                    .into(holder.image);
+        }
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return photos.size();
     }
+
+
 
 
 
--- app/src/main/java/ntnu/no/activity/ItemDetailActivity.java
+++ app/src/main/java/ntnu/no/activity/ItemDetailActivity.java
@@ -1,17 +1,34 @@
 package ntnu.no.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import ntnu.no.R;
+import ntnu.no.RecyclerAdapter;
+import ntnu.no.ViewPagerAdapter;
+import ntnu.no.model.Photo;
 
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class ItemDetailActivity extends AppCompatActivity {
+    private ArrayList<Photo> photos;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_detail);
+
+
+        recyclerView = findViewById(R.id.viewPager);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(photos);
+        recyclerView.setAdapter(adapter);
+
+
     }
 
 
