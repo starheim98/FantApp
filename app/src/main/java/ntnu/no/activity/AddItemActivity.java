35b7f0e26fb092b79c1b956e090a2d537ddb9b4e
Andreas <andreas.hernas@gmail.com>
2021-09-26 20:29:07 +0200

    add item in progress

--- app/src/main/java/ntnu/no/activity/AddItemActivity.java
+++ app/src/main/java/ntnu/no/activity/AddItemActivity.java
@@ -1,5 +1,6 @@
 package ntnu.no.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
@@ -8,35 +9,58 @@ import ntnu.no.model.User;
 import ntnu.no.model.UserObserver;
 import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StrictMode;
+import android.provider.ContactsContract;
 import android.provider.MediaStore;
+
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 public class AddItemActivity extends AppCompatActivity implements UserObserver {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     private LinearLayout imageContainer;
     static final String FILEPROVIDER = "ntnu.no.fileprovider";
-    static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    private List<File> photoFiles;
+    private List<File> photoFiles = new ArrayList<>();
+    private List<ImageView> selected = new ArrayList<>();
     private File currentPhoto;
     private TextView username;
+    private Button removeSelectedBtn;
+    private HashMap<ImageView, File> image_file_map = new HashMap<>();
 
-
+    Uri photoURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,36 +75,29 @@ public class AddItemActivity extends AppCompatActivity implements UserObserver {
         findViewById(R.id.signOutBtn).setOnClickListener(view -> logout());
 
         imageContainer = findViewById(R.id.imageContainer);
+        removeSelectedBtn = findViewById(R.id.removeSelectedBtn);
+        removeSelectedBtn.setOnClickListener(view -> removeImages());
 
-        findViewById(R.id.addImageBtn).setOnClickListener(view -> onCameraClick());
         findViewById(R.id.addItemBtn).setOnClickListener(view -> addItem());
         findViewById(R.id.cancelBtn).setOnClickListener(view -> finish());
+        findViewById(R.id.addImageBtn).setOnClickListener(view -> onCameraClick());
     }
 
-    private void addItem() {
-
-
-        String title = findViewById(R.id.addItemTitle).toString();
-        String price = findViewById(R.id.addItemPrice).toString();
-        String desc = findViewById(R.id.addItemDesc).toString();
-
-
-
-
-    }
-
-    private void logout() {
-        User.getInstance().clearAll();
-        finish();
+    private void removeImages() {
+        for (ImageView imageView : selected){
+            imageContainer.removeView(imageView);
+            photoFiles.remove(image_file_map.get(imageView));
+            image_file_map.remove(imageView);
+        }
+        removeSelectedBtn.setEnabled(false);
     }
 
     public void onCameraClick() {
-        System.out.println("test");
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (intent.resolveActivity(getPackageManager()) != null) {
             currentPhoto = createImageFile();
             if (currentPhoto != null) {
-                Uri photoURI = FileProvider.getUriForFile(this, FILEPROVIDER, currentPhoto);
+                photoURI = FileProvider.getUriForFile(this, FILEPROVIDER, currentPhoto);
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
             }
@@ -105,14 +122,50 @@ public class AddItemActivity extends AppCompatActivity implements UserObserver {
         return result;
     }
 
+    private void addItem() {
+        String title = findViewById(R.id.addItemTitle).toString();
+        String price = findViewById(R.id.addItemPrice).toString();
+        String desc = findViewById(R.id.addItemDesc).toString();
+
+
+        OkHttpClient httpClient = new OkHttpClient();
+    }
+
+    private void logout() {
+        User.getInstance().clearAll();
+        finish();
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        System.out.println("wtfreak");
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
             try {
                 if(requestCode == REQUEST_IMAGE_CAPTURE) {
+                    System.out.println(currentPhoto);
+                    ImageView image = new ImageView(this);
+                    image.setImageURI(photoURI);
+                    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(300, 300);
+                    image.setLayoutParams(layoutParams);
+                    image.setOnClickListener(view -> {
+                        if (selected.contains(image)){
+                            selected.remove(image);
+                            image.setPadding(0,0,0,0);
+                            if (selected.size() == 0) {
+                                removeSelectedBtn.setEnabled(false);
+                            }
+                        } else {
+                            selected.add(image);
+                            removeSelectedBtn.setEnabled(true);
+                            //TODO: fix border
+                            image.setPadding(5, 5, 5, 5);
+                            image.setBackgroundColor(0xff0000);
+                        }
+                    });
+
+                    imageContainer.addView(image);
                     photoFiles.add(currentPhoto);
+                    image_file_map.put(image, currentPhoto);
                     currentPhoto = null;
                 }
             } catch (Exception e) {
